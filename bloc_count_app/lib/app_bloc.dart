6d2bd9d4931a51ba60ed52e9bf27edba837b3f7e
arif <mdarifhosain214@gmail.com>
2025-05-22 12:17:47 +0600

    updated and add decreament part add

--- bloc_count_app/lib/app_bloc.dart
+++ bloc_count_app/lib/app_bloc.dart
@@ -3,9 +3,12 @@ import 'package:bloc_count_app/app_states.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppBloc extends Bloc<AppEvents, AppStates> {
-  AppBloc() : super(InitState()) {
+  AppBloc() : super(AppStates(counter: 0)) {
     on<Increament>((event, emit) {
       emit(AppStates(counter: state.counter + 1));
+    });
+     on<Decreament>((event, emit) {
+      emit(AppStates(counter: state.counter - 1));
     });
   }
 }
